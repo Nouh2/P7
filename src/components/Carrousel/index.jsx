60e9ff55cb13b8ce44cc2e5a84435bfbc1bf5224
noe <noe.tehraoui1@gmail.com>
2024-06-07 20:45:51 +0200

    [ADD] page Home done, page logement and carrousel start

---
+++ src/components/Carrousel/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+
+//on recupere l'id direct dans logements donc on passe id comme props
+//afficher l'image en grand avec les boutons next et previous
+//on doit pouvoir naviguer entre les photos
+
+
+export default function Carrousel() {
+    
+}
--- src/pages/Logements/index.jsx
+++ src/pages/Logements/index.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+//import { useParams } from "react-router-dom";
+import Datas from "../../data/logements.json";
 
-export default function Logements(){
-    return (
-        <div className="container">
-            <h1>Nos logements</h1>
-            <p>Découvrez tous nos logements disponibles.</p>
-            <Link className="link" to="/about">Retour à l'accueil</Link>
+
+//recup l'id du logement
+//a partir de l'id recup les infos du logement
+//creer les composants pour afficher les infos du logement
+
+//const {id} = useParams();
+//const logementData = Datas.filter(data => data.id === id);
+
+{/*export default function Logements() {
+    //Carousel
+    <div className="logement">
+        <div className="logement_content">
+            <div className="logement_info">
+                <h1>{logementData.title}</h1>
+                <p>{logementData.location}</p>
+                <div>
+                    logementData.tags
+                </div>
+            </div>
+            <div className="logement_host_name">
+                logementData.host.name
+            </div>
+            <img src={logementData.cover} alt={logementData.host.name} />
         </div>
-    );
-}
+    </div>
+}*/}
+
+export default function Logements() {};
