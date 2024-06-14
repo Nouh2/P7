82159b8a1d326a15d86b9bb5ca562b44e61846f3
noe <noe.tehraoui1@gmail.com>
2024-06-14 19:55:19 +0200

    [ADD] modif page logement

--- src/pages/Logements/index.jsx
+++ src/pages/Logements/index.jsx
@@ -34,7 +34,6 @@ export default function Logements() {
             <div className="logement_host_name">
                 
             </div>
-            <img src={logementData.cover} alt={logementData.host.name} />
         </div>
     </div>
     </>
