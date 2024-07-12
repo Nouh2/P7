7f54a3ca81429887ba1edfaa4ade1a974f8c7fe7
noe <noe.tehraoui1@gmail.com>
2024-07-12 20:27:02 +0200

    media querie 404

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -3,11 +3,18 @@ import { Link } from "react-router-dom";
 import "../ErrorPage/index.scss"
 
 export default function ErrorPage() {
-    return (
-        <div className="container">
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <Link className="link" to="/">Retourner sur la page d'accueil</Link>
+	return (
+        <>
+		<div className='notFound'>
+			<div className="notFound_infos">
+				<h1 className='notFound_infos_title'>404</h1>
+				<p className='notFound_infos_content'>Oups! La page que vous recherchez n'existe pas.</p>
+			</div>
+				<Link className='notFound_infos_return' to='/'>Retourner sur la page d'accueil</Link>
+		</div>
+        <div className='notFoundFooter'>
+            
         </div>
-    );
+        </>
+	)
 }
